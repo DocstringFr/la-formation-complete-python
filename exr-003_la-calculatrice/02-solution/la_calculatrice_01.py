f0f2b395c6a8dae4cfe5d66ad6fb35c6bed3c0da
Thibault <houdon.thibault@gmail.com>
2020-04-01 15:51:09 +0200

    Fix exercice 003 solution

--- exr-003_la-calculatrice/02-solution/la_calculatrice_01.py
+++ exr-003_la-calculatrice/02-solution/la_calculatrice_01.py
@@ -1,4 +1,4 @@
-a = input("Entrez un premier nombre : ")
-b = input("Entrez un deuxième nombre : ")
+a = int(input("Entrez un premier nombre : "))
+b = int(input("Entrez un deuxième nombre : "))
 resultat = f"Le résultat de l'addition de {a} avec {b} est égal à {a + b}"
 print(resultat)
