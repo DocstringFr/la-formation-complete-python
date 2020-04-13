fc9ab08e9982d4cc4c766aafeac43a31a3bce875
Thibault Houdon <houdon.thibault@gmail.com>
2020-04-13 10:29:49 +0200

    Update devine_un_nombre_02.py

--- exr-007_nombre-mystere-comparaison/01-sources/devine_un_nombre_02.py
+++ exr-007_nombre-mystere-comparaison/01-sources/devine_un_nombre_02.py
@@ -1,8 +1,5 @@
 nombre_mystere = 7
-
 nombre_utilisateur = input("Quel est le nombre mystère ? ")
 
-resultat = int(nombre_utilisateur) == nombre_mystere
-print(resultat)
-
-# Afficher si le nombre entré par l'utilisateur est plus grand, plus petit ou égal au nombre mystère.
+# Afficher à l'aide d'une structure conditionnelle si le nombre entré par l'utilisateur est plus grand,
+# plus petit ou égal au nombre mystère.
